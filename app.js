59b7d15d7b385209002bdae7e7bdb0cd7694c298
Mauregina <mauraregina@gmail.com>
2022-05-11 10:19:17 -0400

    add handlebars to the project
    Signed-off-by: Mauregina <mauraregina@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const app = express()
+const path = require('path')
 const db = require('./db/connection')
 const Job = require('./models/Job')
 const bodyParser = require('body-parser')
@@ -13,6 +15,14 @@ app.listen(PORT, ()=>{
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// handle bars
+app.set('views', path.join(__dirname, 'views')) // location of the templates of the project
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'})) // main layout file application
+app.set('view engine', 'handlebars') // framework we will use to render the views
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 db.authenticate()
     .then(()=>{
         console.log('Database connected successfuly!')
@@ -22,3 +32,7 @@ db.authenticate()
     }) 
 
 app.use('/jobs', require('./routes/jobs'))
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
--- routes/jobs.js
+++ routes/jobs.js
@@ -6,6 +6,10 @@ router.get('/test', (req, res) => {
     res.send('teste')
 })
 
+router.get('/add', (req, res) => {
+    res.render('add')
+})
+
 router.post('/add', (req, res) => {
     let {title, description, salary, company, mail, new_job} = req.body;
 
